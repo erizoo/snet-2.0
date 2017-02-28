9849bfa5d7804b27f52328eaf10e7f8fa842064f
erizo_s <alexboiko1993@gmail.com>
2017-02-28 10:40:13 +0300

    test

--- src/main/java/by/boiko/snet/config/application/ApplicationTestConfig.java
+++ src/main/java/by/boiko/snet/config/application/ApplicationTestConfig.java
@@ -1,7 +1,7 @@
 package by.boiko.snet.config.application;
 
 import by.boiko.snet.model.User;
-import com.mysql.jdbc.Driver;
+import org.apache.commons.dbcp.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -18,7 +17,8 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -32,18 +32,34 @@ public class ApplicationTestConfig {
     @Autowired
     private Environment environment;
 
+//    @Bean
+//    public DataSource dataSource() throws SQLException {
+//        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+//        dataSource.setDriver(new Driver());
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/test?characterEncoding=utf8");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("");
+//        return dataSource;
+//    }
+
     @Bean
-    public DataSource dataSource() throws SQLException {
-        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-        dataSource.setDriver(new Driver());
-        dataSource.setUrl("jdbc:mysql://localhost:3306/test?characterEncoding=utf8");
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
-        return dataSource;
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("JAWSDB_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
     }
 
     @Bean
-    public EntityManagerFactory entityManagerFactory() throws SQLException {  //TODO убрать
+    public EntityManagerFactory entityManagerFactory() throws SQLException, URISyntaxException {  //TODO убрать
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
         factoryBean.setDataSource(dataSource());
         factoryBean.setPackagesToScan("by.boiko.snet.model");
@@ -55,7 +71,7 @@ public class ApplicationTestConfig {
     }
 
     @Bean
-    public LocalSessionFactoryBean sessionFactory() throws SQLException {
+    public LocalSessionFactoryBean sessionFactory() throws SQLException, URISyntaxException {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
         sessionFactory.setPackagesToScan("by.boiko.snet.model");
