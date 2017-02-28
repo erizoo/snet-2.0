39e8d2b9d409c0db1d246a02a4e08264d547b0af
erizo_s <alexboiko1993@gmail.com>
2017-02-28 19:15:21 +0300

    edti controller pdf

--- src/main/java/by/boiko/snet/controller/FileDownloadController.java
+++ src/main/java/by/boiko/snet/controller/FileDownloadController.java
@@ -7,6 +7,7 @@ import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,29 +21,30 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Controller
-
+@RequestMapping("/download")
 public class FileDownloadController
 {
-    @RequestMapping(value = "/reports/users", method = RequestMethod.GET)
+    @RequestMapping("/pdf/{fileName:.+}")
     public void downloadPDFResource( HttpServletRequest request,
-                                     HttpServletResponse response) throws FileNotFoundException, DocumentException {
-        //If user is not authorized - he should be thrown out from here itself
-        String fileName = "users.pdf";
-        //Authorized user will download the file
-        String dataDirectory = request.getServletContext().getRealPath("/users.pdf");
-        Path file = Paths.get(dataDirectory, fileName);
+                                     HttpServletResponse response,
+                                     @PathVariable("fileName") String fileName,
+                                     @RequestHeader String referer) throws DocumentException, FileNotFoundException {
+
         Document document = new Document(PageSize.A4, 50, 50, 50, 50);
-        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(dataDirectory));
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("C:\\ITextTest.pdf"));
+        writer.open();
         document.open();
-        document.add(new Paragraph("A Hello World PDF document."));
+        document.add(new Paragraph("sdgsdgsdg"));
         document.close();
         writer.close();
 
+        //Authorized user will download the file
+        String dataDirectory = request.getServletContext().getRealPath("/WEB-INF/downloads/pdf/");
+        Path file = Paths.get(dataDirectory, fileName);
         if (Files.exists(file))
         {
             response.setContentType("application/pdf");
             response.addHeader("Content-Disposition", "attachment; filename="+fileName);
-
             try
             {
                 PdfWriter.getInstance(document, response.getOutputStream());
