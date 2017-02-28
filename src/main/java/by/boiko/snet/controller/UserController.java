81f69fc0fd1ee52be7bddac097d6af15e0590d90
erizo_s <alexboiko1993@gmail.com>
2017-02-28 11:18:18 +0300

    test

--- src/main/java/by/boiko/snet/controller/UserController.java
+++ src/main/java/by/boiko/snet/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.List;
  * The controller determines methods for access to User service.
  */
 
-@CrossOrigin(origins = "https://serene-wave-27221.herokuapp.com", maxAge = 3600)
+//@CrossOrigin(origins = "https://serene-wave-27221.herokuapp.com", maxAge = 3600)
 @Controller
 public class UserController {
 
