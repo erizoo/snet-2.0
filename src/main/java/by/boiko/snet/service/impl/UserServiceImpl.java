7edb2b217088a941f4ee5d478dfb12a142888b8e
erizo_s <alexboiko1993@gmail.com>
2017-03-14 13:57:21 +0300

    add security config

--- src/main/java/by/boiko/snet/service/impl/UserServiceImpl.java
+++ src/main/java/by/boiko/snet/service/impl/UserServiceImpl.java
@@ -20,6 +20,7 @@ import java.util.List;
 import static com.monitorjbl.json.Match.match;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserDao userDao;
@@ -38,31 +39,26 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    @Transactional
     public List<User> getAll(int offset, int limit) {
         return userDao.loadAllWithOffsetAndLimit(offset, limit);
     }
 
     @Override
-    @Transactional
     public User getAllForId(int id) {
         return userDao.loadAllUsersForId(id);
     }
 
     @Override
-    @Transactional
     public void delete(int id) {
         userDao.delete(id);
     }
 
     @Override
-    @Transactional
     public void save(User user) {
         userDao.save(user);
     }
 
     @Override
-    @Transactional
     public void update(User user) {
         userDao.update(user);
     }
@@ -73,25 +69,22 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    @Transactional
     public List<User> getAll() {
         return userDao.loadAll();
     }
 
     @Override
-    @Transactional
     public LocalDateTime getCreatedDate(int id) {
         return userDao.getCreatedDate(id);
     }
 
     @Override
-    @Transactional
     public String getAllWithParams(Integer offset, Integer limit, String exc, String inc) throws JsonProcessingException {
         StringSplit stringSplit = new StringSplit();
         List<User> userList = userDao.loadAllWithOffsetAndLimit(offset, limit);
         ObjectMapper mapper = new ObjectMapper().registerModule(new JsonViewModule());
         if (exc != null && inc == null) {
-            String[] str = stringSplit.stringSplit(exc);h
+            String[] str = stringSplit.stringSplit(exc);
             return mapper.writeValueAsString(JsonView.with(userList)
                     .onClass(User.class, match()
                             .exclude("*")
@@ -108,7 +101,6 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    @Transactional
     public UserForTwitter findByLogin(String login) {
         return userDao.findByLogin(login);
     }
